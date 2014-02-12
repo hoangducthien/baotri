51b7c57fd569b5cc05fd796c164155776ce803b2
hoang thien <d.thien.mmt04@gmail.com>
2014-02-12 22:36:46 +0700

    loading

--- www/js/jquery_plus.js
+++ www/js/jquery_plus.js
@@ -1,6 +1,8 @@
 var y = $(window).height();
 var x = $(window).width();
 $(function(){ 
+	$("#container").css("width",x); 
+	$("#container").css("height",y);
 	$('.mainloading').css('height', y - 35);
 	$('.mainloading').css('width', x);
 });
