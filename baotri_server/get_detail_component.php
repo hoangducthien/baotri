3d806228aa5f0ca43c56a39fcc468e56dc10c126
hoang thien <d.thien.mmt04@gmail.com>
2014-02-15 19:22:24 +0700

    aaaaaaaaaaaaaa

---
+++ baotri_server/get_detail_component.php
@@ -0,0 +1,28 @@
+<?php
+	if ((isset($_GET['type'])) && (isset($_GET['ms']))) {
+		include('connect_db.php');
+		if ($_GET['type'] == 1) {
+			$res = $mysqli->query("SELECT LoaiTB, ViTri, TrangThai, KieuDang,
+										 HangSX, NuocSX, LoSX, NamSX, ThoiGianBatDauSD
+									FROM tablethietbi
+									WHERE MaTB ='".$_GET['ms']."'");
+			$res->data_seek(0);
+			while ($row = $res->fetch_assoc()) {
+				echo json_encode(array('noidat'=>$row['ViTri'],'trangthai'=>$row['TrangThai'],
+									'kieudang'=>$row['KieuDang'],'hangsx'=>$row['HangSX'],'nuocsx'=>$row['NuocSX'],
+									'namsx'=>$row['NamSX'],'thoigiansd'=>$row['ThoiGianBatDauSD']));
+			}
+			mysqli_free_result($res);
+		} else if ($_GET['type'] == 3) {
+			$res = $mysqli->query("SELECT ThoiGianBaoTri, Level
+									FROM tableloaichitiet
+									WHERE MaSo = '".$_GET['ms']."'");
+			$res->data_seek(0);
+			while ($row = $res->fetch_assoc()) {
+				echo json_encode(array('thoigianbaotri'=>$row['ThoiGianBaoTri'],'level'=>$row['Level']));
+			}
+			mysqli_free_result($res);
+		}
+		mysqli_close($mysqli);
+	}
+?>
