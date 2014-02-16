d7395b904d04b7d2d2cf0756567fb0d98a0eb911
hoang thien <d.thien.mmt04@gmail.com>
2014-02-16 10:32:08 +0700

    lùi gd 2 và 4 phiếu bảo trì

--- baotri_server/set_phieubaotri_return.php
+++ baotri_server/set_phieubaotri_return.php
@@ -1,13 +1,20 @@
 <?php
 	if ((isset($_POST['id'])) && (isset($_POST['gd']))) {
 	include('connect_db.php');
-	$gd = $_POST['gd'] - 1;
+	$gd = $_POST['gd'];
 	$id = $_POST['id'];
-	$kq = 0;	
-	$res = $mysqli->query("UPDATE bienbansuachua
-							SET GiaiDoan = ". $gd ."'
-							WHERE ID = ".$id);
-	if ($res) $kq = 1;	 
+	$kq = 0;
+	if ($gd == 2){
+		$res = $mysqli->query("UPDATE bienbansuachua
+								SET GiaiDoan = 1
+								WHERE ID = ".$id);
+		if ($res) $kq = 1;
+	} else if ($gd == 4){
+		$res = $mysqli->query("UPDATE bienbansuachua
+								SET GiaiDoan = 3
+								WHERE ID = ".$id);
+		if ($res) $kq = 1;
+	}
 	echo json_encode(array('r'=>$kq));
 	mysqli_close($mysqli);
 	}
